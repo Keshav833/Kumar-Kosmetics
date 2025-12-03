595d776c9493fc2d5d0fbcf0cd464fd12a3c801c
keshu <korplz1408@gmail.com>
2025-12-03 20:26:03 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 app.use(cors({
-	origin: "http://localhost:3000",
+	origin: process.env.FRONTEND_URL,
 	credentials: true,
 }));
 
